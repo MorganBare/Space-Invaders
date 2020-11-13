a9bc1b560438613c1a5195f2c7cc6ddd3671f746
MorganBare <morganjbare@gmail.com>
2020-11-13 14:21:22 +0100

    Add background color

--- space-invaders.py
+++ space-invaders.py
@@ -11,19 +11,25 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set background color
+        self.bg_color = (230, 230, 230)
     
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            #watch for keyboard and mouse events
+            # watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw screen during each pass through the loop
+            self.screen.fill(self.bg_color)
         
-        #Make most recently drawn screen visible
+        # Make most recently drawn screen visible
         pygame.display.flip()
 
 if __name__ == "__main__":
-    #Make a game instance and run the game
+    # Make a game instance and run the game
     ai = AlienInvasion()
     ai.run_game()
