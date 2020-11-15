ca9df31018113630e8d24933224c42bb757cf036
MorganBare <morganjbare@gmail.com>
2020-11-15 16:15:10 +0100

    Allowing ship continuous movement to the right

--- ship.py
+++ ship.py
@@ -9,11 +9,19 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
-        self.image = pygame.image.load('Space-Invaders/Space-Invaders/images/rocket.bmp')
+        self.image = pygame.image.load('images/rocket.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement flag
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """Draw the ship at it's current location"""
