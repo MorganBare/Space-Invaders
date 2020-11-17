e3e1238399e133d34aaf41e3241bde30c8b7b50d
MorganBare <morganjbare@gmail.com>
2020-11-17 12:54:00 +0100

    created an instance for the alien

--- space-invaders.py
+++ space-invaders.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien 
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -13,7 +14,6 @@ class AlienInvasion:
         """Initialize the game and create game resources"""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
@@ -21,6 +21,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # Set background color
         self.bg_color = (230, 230, 230)
@@ -65,6 +68,7 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
     def _update_bullets(self):
         """ Update position of bullets and get rid of old bullets"""
         # Update bullet positions
@@ -74,6 +78,12 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    
+    def _create_fleet(self):
+        """Create a fleet of aliens"""
+        # Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
 
                     
     def _update_screen(self):
@@ -82,6 +92,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # Make most recently drawn screen visible
         pygame.display.flip()
 
