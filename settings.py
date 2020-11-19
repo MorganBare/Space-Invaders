51f6ed080b1ce71e7f9396dcaf619f69959438aa
MorganBare <morganjbare@gmail.com>
2020-11-19 20:35:36 +0100

    Game ends when aliens make it to the bottom of the screen

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class Settings:
 
         # ship settings
         self.ship_speed = 3
+        self.ship_limit = 3
 
         # bullet settings
         self.bullet_speed = 1.5
@@ -19,6 +20,6 @@ class Settings:
         self.bullets_allowed = 20
 
         # Alien settings
-        self.alien_speed = 1.0
+        self.alien_speed = 2.0
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
