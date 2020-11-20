0b1cf0362f77639472fdaf31684f3f7e95febee5
MorganBare <morganjbare@gmail.com>
2020-11-20 12:52:06 +0100

    Play button works and mouse cursor hidden

--- space-invaders.py
+++ space-invaders.py
@@ -58,6 +58,27 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        # Start a new game when button clicked
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            self.stats.reset_stats()
+            self.stats.game_active = True 
+
+            # Get rid of any remaining bullets and aliens
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center ship
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the moue cursor
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Responds to keypresses"""
@@ -173,6 +194,7 @@ class AlienInvasion:
 
         else:
             self.stats.game_active = False 
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """ Check if any aliens have reached the bottom of the screen """
