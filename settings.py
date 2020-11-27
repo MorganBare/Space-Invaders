c31ae6433439c852b19d32e21f7254b5851f2faf
MorganBare <morganjbare@gmail.com>
2020-11-27 13:42:49 +0100

    Level is now displayed

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
 
         # ship settings
         self.ship_speed = 3
-        self.ship_limit = 3
+        self.ship_limit = 2
 
         # bullet settings
         self.bullet_speed = 1.5
