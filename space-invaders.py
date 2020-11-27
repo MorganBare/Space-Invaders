a65d7369e7887e7564959bd5088f6b92afb1f37d
MorganBare <morganjbare@gmail.com>
2020-11-27 12:04:08 +0100

    Scoreboard can update itself, increase point value in higher levels, rounds the score

--- space-invaders.py
+++ space-invaders.py
@@ -71,6 +71,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True 
+            self.sb.prep_score()
 
             # Get rid of any remaining bullets and aliens
             self.aliens.empty()
@@ -122,6 +123,11 @@ class AlienInvasion:
         # Check for any bullets that have hit aliens
         # If so, get rid of the bullet and the alien
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet
