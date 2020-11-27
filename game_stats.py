bc671fab1185506b6f6b5429478cbe981a6a7cda
MorganBare <morganjbare@gmail.com>
2020-11-27 13:21:52 +0100

    Highscore added

--- game_stats.py
+++ game_stats.py
@@ -11,6 +11,9 @@ class GameStats:
         # Start game in active state
         self.game_active = False
 
+        # High score should never be reset
+        self.high_score = 0
+
     def reset_stats(self):
         """ Initialize stats that can change during the game """
         self.ships_left = self.settings.ship_limit
